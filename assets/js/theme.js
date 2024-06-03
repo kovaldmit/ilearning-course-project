329111c548c98b591de6a75a34694066f865824e
Dmitry Kovalevsky <kovaldmit.by@gmail.com>
2024-06-03 19:16:35 +0300

    Fixed theme switcher.

--- assets/js/theme.js
+++ assets/js/theme.js
@@ -3,8 +3,9 @@ document.addEventListener('DOMContentLoaded', (event) => {
     const currentTheme = localStorage.getItem('theme') || 'light';
 
     if (currentTheme === 'dark') {
-        document.documentElement.setAttribute('data-bs-theme', 'dark');
         themeToggle.innerHTML = '<i class="fa-solid fa-sun"></i>';
+    } else {
+        themeToggle.innerHTML = '<i class="fa-solid fa-moon"></i>';
     }
 
     themeToggle.addEventListener('click', () => {
